9ece0d6ed28674670d4b9cd07c97870428849825
Guilber Correia <guilber.correia.gc@gmail.com>
2023-08-19 12:18:46 -0300

    Consulta por Id

--- src/main/java/com/project/Game/list/controllers/GameController.java
+++ src/main/java/com/project/Game/list/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.Game.list.dto.GameDTO;
 import com.project.Game.list.dto.GameMinDTO;
 import com.project.Game.list.services.GameService;
 
@@ -21,5 +23,11 @@ public class GameController {
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> lista = gameService.findAll();
         return lista;
+    } 
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO game = gameService.findById(id);
+        return game;
     } 
 }
--- src/main/java/com/project/Game/list/services/GameService.java
+++ src/main/java/com/project/Game/list/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.Game.list.dto.GameDTO;
 import com.project.Game.list.dto.GameMinDTO;
 import com.project.Game.list.entities.Game;
 import com.project.Game.list.repositories.GameRepository;
@@ -18,6 +19,10 @@ public class GameService {
     public List<GameMinDTO> findAll(){
         List<Game> lista = gameRepository.findAll();
         return lista.stream().map(x -> new GameMinDTO(x)).toList();
-
+    }
+     
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
